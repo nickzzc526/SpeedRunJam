d06cbf697c19b382b1df222f102303c5f8ea987f
Nick Zhechuan Zhang <nickzzc@hotmail.com>
2013-09-21 11:25:36 -0400

    1123

---
+++ Gemini/Assets/Script/Star1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star1 : MonoBehaviour {
+	
+	Transform myStar2;
+	public float Speed = 1;
+	public float risingSpeed = 1;
+	
+	// Use this for initialization
+	void Start () {
+	myStar2 = GameObject.FindGameObjectWithTag("Star2").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKey("left")){
+			transform.position += new Vector3(-Speed,0,0);
+		}
+	if(Input.GetKey("right")){
+			transform.position += new Vector3(Speed,0,0);
+		}
+	if(Vector3.Distance(this.transform.position,myStar2.position)<10f){
+			transform.position += new Vector3(0,0,risingSpeed);
+		}
+	}
+}
---
+++ Gemini/Assets/Script/Star2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star2 : MonoBehaviour {
+	
+	Transform myStar1;
+	public float Speed = 1;
+	public float risingSpeed = 1;
+	
+	// Use this for initialization
+	void Start () {
+	myStar1 = GameObject.FindGameObjectWithTag("Star1").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKey(KeyCode.A)){
+			transform.position += new Vector3(-Speed,0,0);
+		}
+	if(Input.GetKey(KeyCode.D)){
+			transform.position += new Vector3(Speed,0,0);
+		}
+	if(Vector3.Distance(this.transform.position,myStar1.position)<3f){
+			transform.position += new Vector3(0,0,risingSpeed);
+		}
+	}
+}
